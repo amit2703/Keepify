6ac0079986eafe66d56921dcac353c9182ed1198
Shubham <babukumar9931@gmail.com>
2020-10-22 20:05:19 +0530

    Added Edit Activity.

--- app/src/main/java/com/example/keepify/MainActivity.java
+++ app/src/main/java/com/example/keepify/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.keepify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAdd;
+
+    public void onAddClicked() {
+        Intent intent = new Intent(this, EditActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnAdd = (Button) findViewById(R.id.btnAdd);
+
+        this.btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAddClicked();
+            }
+        });
     }
 }
