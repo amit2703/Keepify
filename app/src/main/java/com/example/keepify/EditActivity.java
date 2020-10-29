13ac2cf2ca52514dd39405446060935eb9034ad5
Shubham <babukumar9931@gmail.com>
2020-10-29 18:22:02 +0530

    Modified EditActivity.java

--- app/src/main/java/com/example/keepify/EditActivity.java
+++ app/src/main/java/com/example/keepify/EditActivity.java
@@ -3,12 +3,69 @@ package com.example.keepify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.keepify.db.DatabaseAccess;
 
 public class EditActivity extends AppCompatActivity {
+
+    private EditText etText;
+    private Button btnSave;
+    private Button btnCancel;
+    private keepify memo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        this.etText = (EditText) findViewById(R.id.etText);
+        this.btnSave = (Button) findViewById(R.id.btnSave);
+        this.btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null) {
+            memo = (keepify) bundle.get("MEMO");
+            if(memo != null) {
+                this.etText.setText(memo.getText());
+            }
+        }
+
+        this.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSaveClicked();
+            }
+        });
+
+        this.btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCancelClicked();
+            }
+        });
+    }
+
+    public void onSaveClicked() {
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        if(memo == null) {
+            // Add new memo
+            keepify temp = new keepify();
+            temp.setText(etText.getText().toString());
+            databaseAccess.save(temp);
+        } else {
+            // Update the memo
+            memo.setText(etText.getText().toString());
+            databaseAccess.update(memo);
+        }
+        databaseAccess.close();
+        this.finish();
+    }
+
+    public void onCancelClicked() {
+        this.finish();
     }
 }
